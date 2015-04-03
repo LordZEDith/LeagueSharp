5f58ae5b85c285663f684cd99ee5060e1e91c1f3
LordZEDith <vrandan@gmail.com>
2015-04-02 22:40:07 -0700

    Update Program.cs

--- Beasty Nasus/Program.cs
+++ Beasty Nasus/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
+using SharpDX;
 using Color = System.Drawing.Color;
+using LeagueSharp.Common.Data;
 
 class Program
 {
