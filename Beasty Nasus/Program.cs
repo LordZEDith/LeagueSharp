6545dfa98aef2906491bb9056eb0c6dafde53fc7
LordZEDith <vrandan@gmail.com>
2015-04-02 19:57:08 -0700

    Update Program.cs

--- Beasty Nasus/Program.cs
+++ Beasty Nasus/Program.cs
@@ -339,7 +339,7 @@ class Program
             return;
         {
 
-            if (_player.CountEnemiesInRange(ObjectManager.Player, (int)Hydra.Range) >= 1)
+            if (ObjectManager.Player.CountEnemiesInRange(ObjectManager.Player, (int)Hydra.Range) >= 1)
             {
                 Hydra.Cast();
             }
@@ -352,7 +352,7 @@ class Program
             return;
         {
 
-            if (_player.CountEnemiesInRange(ObjectManager.Player, (int)Hydra.Range) >= 1)
+            if (ObjectManager.Player.CountEnemiesInRange(ObjectManager.Player, (int)Hydra.Range) >= 1)
             {
                 Tiamat.Cast();
             }
@@ -365,7 +365,7 @@ class Program
 
         {
 
-            if (_player.CountEnemiesInRange(ObjectManager.Player, (int)E.Range) >= Program.Menu.Item("AutoREA").GetValue<Slider>().Value)
+            if (ObjectManager.Player.CountEnemiesInRange(ObjectManager.Player, (int)E.Range) >= Program.Menu.Item("AutoREA").GetValue<Slider>().Value)
             {
                 R.Cast();
             }
