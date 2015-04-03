340b742974be6f3a9bdb0a6354ffb1ecbb02fb06
LordZEDith <vrandan@gmail.com>
2015-04-02 19:41:16 -0700

    Update Program.cs

--- Beasty Nasus/Program.cs
+++ Beasty Nasus/Program.cs
@@ -182,12 +182,12 @@ class Program
         if (E.IsReady())
         {
             // draw Aqua circle around the player
-            Utility.RenderCircle(Player.Position, Q.Range, Color.Aqua);
+            Utility.Render.Circle(Player.Position, Q.Range, Color.Aqua);
         }
         else
         {
             // draw DarkRed circle around the player while on cd
-            Utility.RenderCircle(Player.Position, Q.Range, Color.DarkRed);
+            Utility.Render.Circle(Player.Position, Q.Range, Color.DarkRed);
         }
     }
 
